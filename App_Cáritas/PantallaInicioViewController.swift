702cbd334874293e0b7b3d1a476df821c24fcdfd
MaryFerfer <a01283983@itesm.mx>
2022-09-07 09:15:19 -0500

    Pull Request Pantallas Iniciales
    Las pantallas iniciales sin código. Incluye segues

---
+++ App_Cáritas/PantallaInicioViewController.swift
@@ -0,0 +1,31 @@
+//
+//  PantallaInicioViewController.swift
+//  App_Cáritas
+//
+//  Created by Mary Fer on 06/09/22.
+//
+
+import UIKit
+
+
+
+class PantallaInicioViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ App_Cáritas/PantallaViewController.h
@@ -0,0 +1,16 @@
+//
+//  PantallaViewController.h
+//  App_Cáritas
+//
+//  Created by Mary Fer on 07/09/22.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface PantallaViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
